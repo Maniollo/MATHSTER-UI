391897c484d2fdca2caf91234fe06524899823b9
maniollo <mariusz.maslanka@gmail.com>
2020-04-29 17:36:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import {TitlePanel} from "./TitlePanel/TitlePanel";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <TitlePanel/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/TitlePanel/TitlePanel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TitlePanel = () => {
+    return (
+        <div className="App-header">
+            <h1>MATHSTER</h1>
+            <h2>BE THE MATH HERO</h2>
+        </div>
+    );
+};
